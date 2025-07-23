5fc28c44ba72e96b0e3a9248e7cb2ad859fc6f3d
ArtyomBataninSuper228 <artyom.batanin3331@gmail.com>
2025-07-23 12:42:06 +0500

    commit

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import numpy as np
-from math import *
 from matplotlib import pyplot
-from pendulum_systems import *
 from integral import *
-import air_systems
+from integration_systems import air_systems
+from integration_systems import pendulum_systems as pend
 
-p1 = air_systems.create_pendulum_sys(0, 1, 1, 1)#create_pendulum_sys(1, -1, 0,0, 1)
+p1 = pend.create_pendulum_sys(0.1, 1, 0,0, 1)
 p2 = copy.copy(p1)
 p3 = copy.copy(p1)
 p4 = copy.copy(p1)
@@ -17,48 +15,53 @@ x3 = []
 x4 = []
 t = []
 time = 0
-h = 10**(-5)
-toe = 100
+h = 10**(-3)
+toe = 10
+get_diff = pend.get_diff
+do_step = pend.do_step
 while time <= toe:
-    p1 = air_systems.do_step(p1, h, air_systems.get_diff(p1))
-    p2 = do_runge_kutta_classic_step(p2, air_systems.get_diff, air_systems.do_step, h)
-    p3 = do_rk4_1_step(p3, air_systems.get_diff, air_systems.do_step, h)
-    p4 = do_rk4_2_step(p4, air_systems.get_diff, air_systems.do_step, h)
+    p1 = do_step(p1, h, get_diff(p1))
+    p2 = do_runge_kutta_classic_step(p2, get_diff, do_step, h)
+    p3 = do_rk4_1_step(p3, get_diff, do_step, h)
+    p4 = do_rk4_2_step(p4, get_diff, do_step, h)
     time += h
     """x.append(p1[0, 0])
     y.append(p1[0, 1])
     x2.append(p2[0, 0])
     y2.append(p2[0, 1])
     """
-    x.append(air_systems.get_true_ans(p1, 1, time)[0] - p1[0, 0])
-    x2.append(air_systems.get_true_ans(p1, 1, time)[0] - p2[0, 0])
-    x3.append(air_systems.get_true_ans(p1, 1, time)[0] - p3[0, 0])
-    x4.append(air_systems.get_true_ans(p1, 1, time)[0] - p4[0, 0])
+    #true_val = air_systems.get_true_ans(p1, 1, time) [0]
+    x.append(p1[0, 0])
+    x2.append(p2[0, 0])
+    x3.append( p3[0, 0])
+    x4.append(p4[0, 0])
     t.append(time)
 
+xaxis = 'time'
+yaxis = 'x'
 
 pyplot.subplot(2, 2, 1)
 pyplot.title('Euler', fontsize=17)
-pyplot.plot(t, x, label = 'euler')
-pyplot.xlabel('time', fontsize=12, color='blue')
-pyplot.ylabel('Error', fontsize=12, color='blue')
+pyplot.plot(t, x)
+pyplot.xlabel(xaxis, fontsize=12, color='blue')
+pyplot.ylabel(yaxis, fontsize=12, color='blue')
 
 pyplot.subplot(2, 2, 2)
 pyplot.title('RK 4 classic', fontsize=17)
-pyplot.plot(t, x2, label = 'euler')
-pyplot.xlabel('time', fontsize=12, color='blue')
-pyplot.ylabel('Error', fontsize=12, color='blue')
+pyplot.plot(t, x2)
+pyplot.xlabel(xaxis, fontsize=12, color='blue')
+pyplot.ylabel(yaxis, fontsize=12, color='blue')
 
 pyplot.subplot(2, 2, 3)
 pyplot.title('my 1', fontsize=17)
-pyplot.plot(t, x3, label = 'euler')
-pyplot.xlabel('time', fontsize=12, color='blue')
-pyplot.ylabel('Error', fontsize=12, color='blue')
+pyplot.plot(t, x3)
+pyplot.xlabel(xaxis, fontsize=12, color='blue')
+pyplot.ylabel(yaxis, fontsize=12, color='blue')
 
 pyplot.subplot(2, 2, 4)
 pyplot.title('my 2', fontsize=17)
-pyplot.plot(t, x4, label = 'euler')
-pyplot.xlabel('time', fontsize=12, color='blue')
-pyplot.ylabel('Error', fontsize=12, color='blue')
+pyplot.plot(t, x4)
+pyplot.xlabel(xaxis, fontsize=12, color='blue')
+pyplot.ylabel(yaxis, fontsize=12, color='blue')
 
 pyplot.show()
